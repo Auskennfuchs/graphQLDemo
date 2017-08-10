a1723c2bcccf49eda66d8bdb51bcddd090f28622
Auskennfuchs <auskennfuchs@auskennfuchs.de>
2017-08-11 00:31:27 +0200

    optimized cache with dataloader

--- schema.js
+++ schema.js
@@ -1,4 +1,7 @@
-const {
+import {makeExecutableSchema} from 'graphql-tools'
+import DataLoader from 'dataloader'
+
+import {
     GraphQLObjectType,
     GraphQLString,
     GraphQLInt,
@@ -7,66 +10,65 @@ const {
     GraphQLNonNull,
     GraphQLError,
     buildSchema
-} = require('graphql')
+} from 'graphql'
 
-const axios = require('axios')
+import axios from 'axios'
 
-const QueryFunctions = {
-    customer: ({id})=> {
-        return axios.get('http://localhost:3000/customers/'+id)
-                .then(res => res.data);
+const typeDefs = `
+    type CustomerType {
+        id: String!
+        name: String
+        email: String
+        age: Int
+        friends: [CustomerType]
+    }
+
+    type Query {
+        customer(id:String): CustomerType
+        customers: [CustomerType]
+    }
+
+    type Mutation {
+        addCustomer(name:String!, email:String!, age:Int): CustomerType
+        deleteCustomer(id:String!): Boolean
+        editCustomer(id:String!,name: String, email: String, age: Int): CustomerType
+    }
+`;
+
+const resolvers = {
+    Query: {
+        customer: (_,args,{loaders})=> {
+            return loaders.customer.load(args.id)
+        },
+        customers: (_,args,{loaders}) => {
+            return loaders.customer.loadAll()
+        },
     },
-    customers: () => {
-        return axios.get('http://localhost:3000/customers')
-            .then(res => res.data)
+    Mutation: {
+        addCustomer: ({name,email,age}) => {
+            console.log(email)
+            return axios.post('http://localhost:3000/customers',{
+                name: name,
+                email: email,
+                age: age
+            })
+                .then(res => res.data)
+        },
+        deleteCustomer: ({id}) => {
+            return axios.delete('http://localhost:3000/customers/'+id)
+                .then(()=>{return true})
+                .catch(() => {return false})
+        },
+        editCustomer: (args)=> {
+            return axios.patch('http://localhost:3000/customers/'+args.id, args)
+                .then(res => res.data)
+        }
     },
-    addCustomer: ({name,email,age}) => {
-        console.log(email)
-        return axios.post('http://localhost:3000/customers',{
-            name: name,
-            email: email,
-            age: age
-        })
-            .then(res => res.data)
-    },
-    deleteCustomer: ({id}) => {
-        return axios.delete('http://localhost:3000/customers/'+id)
-        .then(()=>{return true})
-        .catch(() => {return false})
-    },
-    editCustomer: (args)=> {
-        console.log("Test")
-        return axios.patch('http://localhost:3000/customers/'+args.id,
-            args
+    CustomerType: {
+        friends : (customer,args,{loaders}) => (
+            loaders.customer.loadMany(customer.friends || [])
         )
-        .then(res => res.data)
     }
 }
 
-module.exports = {
-    schema: buildSchema(`
-        type CustomerType {
-            id: String
-            name: String
-            email: String
-            age: Int
-        }
-
-        type Query {
-            customer(id:String): CustomerType
-            customers: [CustomerType]
-        }
-
-        type Mutation {
-            addCustomer(name:String!, email:String!, age:Int): CustomerType
-            deleteCustomer(id:String!): Boolean
-            editCustomer(id:String!,name: String, email: String, age: Int): CustomerType
-        }
-
-        schema {
-            query: Query
-            mutation: Mutation            
-        }
-    `),
-    queryFunctions: QueryFunctions
-}
+export const schema = makeExecutableSchema({typeDefs, resolvers})
